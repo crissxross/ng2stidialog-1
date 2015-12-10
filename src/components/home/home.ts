3c1505e45581b9936bf77640bd7fb976dcc91b2c
Christine Wilks <crissxross@gmail.com>
2015-12-10 17:00:14 +0000

    Add home.html & scene.html & refactor corresponding ts files

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -2,20 +2,7 @@ import {Component, OnInit} from 'angular2/angular2';
 
 @Component({
 	selector: 'home',
-	template: `
-	<div class="hgt90">
-         <div class="row hgt50 bg-home">
-            <div class="ten columns offset-by-one column">
-                <h1>Stitched Up Prototype Dialogue Engine</h1>
-            </div>
-        </div>
-        <div class="row hgt50 bg-police">
-            <div class="ten columns offset-by-one column">
-                <h2>My first prototype in Angular 2</h2>
-            </div>
-        </div>
-    </div>
-	`
+	templateUrl: 'components/home/home.html'
 })
 
 export class HomeComponent implements OnInit {
--- src/components/scene/scene.ts
+++ src/components/scene/scene.ts
@@ -2,21 +2,7 @@ import {Component, OnInit} from 'angular2/angular2';
 
 @Component({
 	selector: 'scene',
-	template: `
-	<div class="hgt90">
-         <div class="row hgt50 bg-han-light">
-            <div class="ten columns offset-by-one column">
-                <h1>Mock Scene</h1>
-				<h2>Hannah speaks here</h2>
-            </div>
-        </div>
-        <div class="row hgt50 bg-joel-light">
-            <div class="ten columns offset-by-one column">
-                <h2>Joel/Sarah will speak & think here</h2>
-            </div>
-        </div>
-    </div>
-	`
+	templateUrl: 'components/scene/scene.html'
 })
 
 export class SceneComponent implements OnInit {
